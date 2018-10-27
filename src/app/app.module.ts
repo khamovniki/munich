3bf95cbf616800ecf1cf10652d7b04834e6a62fd
Maxim Unguryan <unguryan.mo@edu.spbstu.ru>
2018-10-27 16:35:48 +0300

    Setup proxy, create page new-post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {NewPostComponent} from './new-post/new-post.component';
 import {AppRoutingModule} from './app-routing.module';
+import {
+  MatButtonModule,
+  MatCardModule, MatCheckboxModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatSelectModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from '@angular/forms';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +25,19 @@ import {AppRoutingModule} from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    NgxMaterialTimepickerModule.forRoot(),
+    MatIconModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/new-post/new-post.component.ts
+++ src/app/new-post/new-post.component.ts
@@ -1,10 +1,36 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+
+import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+
+import * as moment from 'moment';
+import {HttpClient} from '@angular/common/http';
+// tslint:disable-next-line:no-duplicate-imports
+// import {default as _rollupMoment} from 'moment';
 
 @Component({
   selector: 'app-new-post',
   templateUrl: 'new-post.template.html',
-  styleUrls: ['new-post.style.css']
+  styleUrls: ['new-post.style.css'],
+  providers: [
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS}
+  ]
 })
 export class NewPostComponent {
-
+  postForm = new FormGroup({
+    text: new FormControl(''),
+    tags: new FormControl(''),
+    schedule: new FormControl(false),
+    date: new FormControl(''),
+    time: new FormControl('')
+  });
+  tagList: string[] = ['Lol', 'Kek'];
+  constructor(private http: HttpClient) {
+    this.http.get('api/tags/list').subscribe((tags: [string]) => this.tagList = tags);
+  }
+  sendPost() {
+    console.log(this.postForm.value);
+  }
 }
