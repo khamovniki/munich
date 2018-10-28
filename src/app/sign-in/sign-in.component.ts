0a59d85203c6ec625526d3075edc1783528994fa
Maxim Unguryan <unguryan.mo@edu.spbstu.ru>
2018-10-28 08:38:48 +0300

    Auth part

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -11,7 +13,24 @@ export class SignInComponent {
     username: new FormControl(''),
     password: new FormControl('')
   });
+
+  constructor(private http: HttpClient, private authService: AuthService) {
+  }
+
   signIn() {
     console.log(this.signInForm.value);
+    const {username, password} = this.signInForm.value;
+    this.http.post('api/signIp', {
+      username,
+      password
+    }).subscribe(
+      (body: string) => {
+        console.log(body);
+        this.authService.authorize(body);
+      },
+      () => {
+        console.log('Error');
+      }
+    );
   }
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {AbstractControl, FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-up',
@@ -12,10 +14,23 @@ export class SignUpComponent {
     password: new FormControl(''),
     confirmPassword: new FormControl('')
   });
+  constructor(private http: HttpClient, private router: Router) {}
   signUp() {
     const {username, password, confirmPassword} = this.signUpForm.value;
     if (password === confirmPassword) {
       console.log(username, password);
+      this.http.post('api/signUp', {
+        username,
+        password
+      }).subscribe(
+        () => {
+          console.log('Cool');
+          this.router.navigate(['sign-in']);
+        },
+        () => {
+          console.log('Bad');
+        }
+      );
     } else {
       console.log('Password incorrect');
     }
